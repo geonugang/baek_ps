e19f25613a492a656d9131a87c5649eb96b20f9f
kang geon woo <138636798+geonugang@users.noreply.github.com>
2023-07-20 10:51:29 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,7 +1,4 @@
-num = []
 A, B = map(int, input().split())
 reverse_num1 = int(str(A)[::-1])
 reverse_num2 = int(str(B)[::-1])
-num.append(reverse_num1)
-num.append(reverse_num2)
-print(max(num))
+print(max(reverse_num1, reverse_num2))
